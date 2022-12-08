a707dbff736a43e488182da62ab91df5ec5c55b4
hypensterisk <roykhanalaushaf0@gmail.com>
2022-12-08 07:39:45 +0700

    update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,13 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+import Navbar from './app/navbar'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-const children = <StrictMode></StrictMode>
+const children = (
+  <StrictMode>
+    <Navbar />
+  </StrictMode>
+)
 root.render(children)
